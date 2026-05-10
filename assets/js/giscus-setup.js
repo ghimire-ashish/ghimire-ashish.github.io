e4e27e6c03e9fae827371f2a2d1d6cf870d7510f
ghimire-ashish <seasonashish@gmail.com>
2026-05-10 17:25:29 +0000

    Deploying to gh-pages from @ ghimire-ashish/ghimire-ashish.github.io@f8a86e0e6c6c525de03489edd707dfbeb22e54ec 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "seasonashish/ashishghimire.github.io",
+    "data-repo": "ghimire-ashish/ghimire-ashish.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
