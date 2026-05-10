e87e397cd9c45b088422fcc9bc4159ebe0447d0c
ghimire-ashish <seasonashish@gmail.com>
2026-05-10 21:57:28 +0000

    Deploying to gh-pages from @ ghimire-ashish/ghimire-ashish.github.io@84eab2383919fea10c7d327a9f95bc125e9538fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "About",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
